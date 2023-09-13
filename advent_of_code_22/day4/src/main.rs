c066324f517450c90e35f5aa95f6c679992f73cf
Jakub Turowski <jakub.turowski@nokia.com>
2023-09-13 17:28:58 +0200

    day 4

---
+++ advent_of_code_22/day4/src/main.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+fn main() {
+    let f = fs::read_to_string("input.txt");
+    let file = f.unwrap();
+    let sum: usize = file
+        .split(|c: char| !c.is_numeric())
+        .filter(|token| !token.is_empty())
+        .map(|value| value.parse::<isize>().unwrap())
+        .collect::<Vec<_>>()
+        .chunks_exact(4)
+        .map(|group| {
+            let within = |(al, ar), (bl, br)| -> bool { al >= bl && ar <= br };
+            (within((group[0], group[1]), (group[2], group[3]))
+                || within((group[2], group[3]), (group[0], group[1]))) as usize
+        })
+        .sum();
+    print!("sum: {}", sum);
+}
---
+++ advent_of_code_22/day4_2/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn main() {
+    let f = fs::read_to_string("input.txt");
+    let file = f.unwrap();
+    let sum: usize = file
+        .split(|c: char| !c.is_numeric())
+        .filter(|token| !token.is_empty())
+        .map(|value| value.parse::<isize>().unwrap())
+        .collect::<Vec<_>>()
+        .chunks_exact(4)
+        .map(|group| {
+            let ((al, ar), (bl, br)) = ((group[0], group[1]), (group[2], group[3]));
+            let double_distance = (al+ar).abs_diff((bl+br));
+            let size_sum =  ar.abs_diff(al) + br.abs_diff(bl);
+            (double_distance <= size_sum) as usize
+        })
+        .sum();
+    print!("sum: {}", sum);
+}
